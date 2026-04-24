ed0fb509184df740ecaf125a0a0f042923cf3a44
Yogesh Pal <159111601+theyogeshpal@users.noreply.github.com>
2026-04-24 16:15:22 +0530

    changes

--- obj/Release/net8.0/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Release/net8.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28cc894dff44947b2b269e3c905b5512ccf44fe9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e59826db77da6c479c6acd09423be14b6b69b66d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
